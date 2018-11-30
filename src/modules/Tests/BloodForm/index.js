5838311b826f858875edb8f2094f52eeb6996699
Kirill Cherkalov <kirill.cherkalov@gmail.com>
2018-11-30 23:20:26 +0400

    added immmutabily-helper & fixed form bug

--- src/modules/Tests/BloodForm/index.js
+++ src/modules/Tests/BloodForm/index.js
@@ -2,138 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Text } from 'react-native-paper';
 import { View } from 'react-native';
+import update from 'immutability-helper';
 
 import * as S from './styled';
 
-const Indicators = {
-  glucose: {
-    value: '',
-    info: '',
-    unit: 'ммоль/л',
-    error: '',
-    flag: 'unit',
-    label: 'Глюкоза',
-    limits: {
-      man: {
-        maxLimit: 5.3,
-        minLimit: 3.5,
-      },
-      woman: {
-        maxLimit: 5.3,
-        minLimit: 3.5,
-      },
-    },
-  },
-  hemoglobin: {
-    value: '',
-    info: '',
-    unit: 'g/dL',
-    error: '',
-    flag: 'unit',
-    label: 'Конц. гемоглобина',
-    limits: {
-      man: {
-        maxLimit: 17.5,
-        minLimit: 13,
-      },
-      woman: {
-        maxLimit: 15.7,
-        minLimit: 12,
-      },
-    },
-  },
-  leukocytes: {
-    value: '',
-    info: '',
-    unit: '*10^3/μL',
-    error: '',
-    flag: 'unit',
-    label: 'Конц. лейкоцитов',
-    limits: {
-      man: {
-        maxLimit: 9,
-        minLimit: 4,
-      },
-      woman: {
-        maxLimit: 9,
-        minLimit: 4,
-      },
-    },
-  },
-  erythrocytes: {
-    value: '',
-    info: '',
-    unit: '*10^3/μL',
-    error: '',
-    flag: 'unit',
-    label: 'Кол-во эритроцитов',
-    limits: {
-      man: {
-        maxLimit: 6.08,
-        minLimit: 4.9,
-      },
-      woman: {
-        maxLimit: 5.1,
-        minLimit: 3.7,
-      },
-    },
-  },
-  hematocrit: {
-    value: '',
-    info: '',
-    unit: '%',
-    flag: 'unit',
-    label: 'Гематокрит',
-    limits: {
-      man: {
-        maxLimit: 50,
-        minLimit: 39,
-      },
-      woman: {
-        maxLimit: 47,
-        minLimit: 35,
-      },
-    },
-  },
-  numbPlatelets: {
-    value: '',
-    info: '',
-    unit: '*10^3/μL',
-    error: '',
-    flag: 'unit',
-    label: 'Кол-во тромбоцитов',
-    limits: {
-      man: {
-        maxLimit: 400,
-        minLimit: 150,
-      },
-      woman: {
-        maxLimit: 400,
-        minLimit: 150,
-      },
-    },
-  },
-  esr: {
-    value: '',
-    info: '',
-    unit: 'мм/час',
-    error: '',
-    flag: 'unit',
-    label: 'СОЭ',
-    limits: {
-      man: {
-        maxLimit: 15,
-        minLimit: 2,
-      },
-      woman: {
-        maxLimit: 20,
-        minLimit: 2,
-      },
-    },
-  },
-};
-
-const Indicators2 = [
+const Indicators = [
   {
     id: 'glucose',
     value: '',
@@ -271,96 +144,63 @@ const Indicators2 = [
 class BloodForm extends Component {
   static propTypes = {
     navigation: PropTypes.shape({}).isRequired,
-    // indicators: PropTypes.shape({}).isRequired,
   }
 
   static navigationOptions = () => ({ title: 'Анализ Крови' });
 
-  state = { indicators: [...Indicators2], sex: 'man' }
+  state = { indicators: [...Indicators], sex: 'man' }
 
-  handleChange = (key, value, indx) => {
+  handleChange = (value, indx) => {
     const { indicators } = this.state;
-    // if (elem.flag === 'error' || elem.flag === 'info') {
-    //   // indicators.map(elem => {
-    //   //   if (elem.flag === )
-    //   // })
-    //   return this.setState({
-    //     indicators: indicators.map(elem => (elem.id === key ? { ...elem, value } : elem)),
-    //   });
-    // }
-    return this.setState({
-      indicators: indicators.map(elem => (elem.id === key ? { ...elem, value } : elem)),
-    });
+    const elem = indicators[indx];
 
-    //   return this.setState(({ indicators }) => ({
-    //     indicators: {
-    //       ...indicators,
-    //       [key]: {
-    //         ...elem,
-    //         flag: 'unit',
-    //         error: '',
-    //         value,
-    //       },
-    //     },
-    //   }));
-    // }
+    if (elem.flag === 'error' || elem.flag === 'info') {
 
-    // return this.setState(
-    //   ({ indicators }) => ({
-    //     indicators: {
-    //       ...indicators,
-    //       [key]: { ...elem, value },
-    //     },
-    //   }),
-    // );
+      return this.setState(({ indicators }) => ({
+        indicators: update(indicators, { [indx]: {
+          value: { $set: value },
+          flag: { $set: 'unit' },
+          error: { $set: '' } },
+        }),
+      }));
+    }
+
+    return this.setState(({ indicators }) => ({
+      indicators: update(indicators, { [indx]: { value: { $set: value } } }),
+    }));
   }
 
-  showEmptyStringError = el => this.setState(({ indicators }) => ({
-    indicators: {
-      ...indicators,
-      [el]: {
-        ...indicators[el],
-        flag: true,
-        error: 'Пустая строка',
-      },
-    },
-  }))
-
-  showInfo = (el, data) => this.setState(({ indicators }) => ({
-    indicators: {
-      ...indicators,
-      [el]: {
-        ...indicators[el],
-        ...data,
-      },
-    },
+  showInfo = (indx, { flag, error = '', info = '' }) => this.setState(({ indicators }) => ({
+    indicators: update(indicators, { [indx]: {
+      flag: { $set: flag },
+      error: { $set: error },
+      info: { $set: info },
+    } }),
   }))
 
   handleClick = () => {
     const { indicators, sex } = this.state;
 
-    Object.keys(indicators).forEach((el) => {
-      const { limits } = indicators[el];
-      const elem = indicators[el];
-      const { value } = elem;
+    indicators.forEach((el, i) => {
+      const { limits, value } = el;
 
       if (!value) {
-        return this.showInfo(el, { flag: 'error', error: 'Пустая строка' });
+        return this.showInfo(i, { flag: 'error', error: 'Пустая строка' });
       }
 
       if (Number.isNaN(parseInt(value, 10))) {
-        return this.showInfo(el, { flag: 'error', error: 'Недопустимый формат' });
+        return this.showInfo(i, { flag: 'error', error: 'Недопустимый формат' });
       }
 
       if (value > limits[sex].maxLimit) {
-        return this.showInfo(el, { flag: 'info', info: 'Превшена норма' });
+        return this.showInfo(i, { flag: 'info', info: 'Превшена норма' });
       }
 
       if (value < limits[sex].minLimit) {
-        return this.showInfo(el, { flag: 'info', info: 'Значение ниже нормы' });
+        return this.showInfo(i, { flag: 'info', info: 'Значение ниже нормы' });
       }
 
-      return this.showInfo(el, { flag: 'info', info: 'Норма' });
+      return this.showInfo(i, { flag: 'info', info: 'Норма' });
     });
   }
 
@@ -393,7 +233,7 @@ class BloodForm extends Component {
                     label={label}
                     value={value}
                     error={error && error}
-                    onChangeText={text => this.handleChange(`${id}`, text, indx)}
+                    onChangeText={text => this.handleChange(text, indx)}
                   />
                   <S.UnitText
                     type={flag === 'unit' || flag === 'info' ? 'info' : 'error'}
