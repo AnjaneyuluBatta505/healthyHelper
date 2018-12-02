d524432166fd993c1f47080820b03899a3d23d81
Kirill Cherkalov <kirill.cherkalov@gmail.com>
2018-12-02 16:18:24 +0400

    added Icons & new List view for components

--- src/modules/Tests/index.js
+++ src/modules/Tests/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { List } from 'react-native-paper';
 
 import * as S from './styled';
 
@@ -8,21 +9,25 @@ const analysis = [
     id: `$_${Math.random().toString(36).substr(2, 9)}`,
     value: 'Анализ мочи',
     route: 'wee',
+    iconName: 'urine',
   },
   {
     id: `$_${Math.random().toString(36).substr(2, 9)}`,
     value: 'Анализ крови',
     route: 'BloodForm',
+    iconName: 'testtube',
   },
   {
     id: `$_${Math.random().toString(36).substr(2, 9)}`,
     value: 'Витамины',
     route: 'vitamins',
+    iconName: 'vitamins',
   },
   {
     id: `$_${Math.random().toString(36).substr(2, 9)}`,
     value: 'Гормоны',
     route: 'HormonesForm',
+    iconName: 'balance',
   },
 ];
 
@@ -33,7 +38,15 @@ class Tests extends Component {
 
   static navigationOptions = () => ({ title: 'Analyses' });
 
-  handleClick = (_id) => {
+  state = {
+    expanded: true,
+  }
+
+  handlePress = () => this.setState(({ expanded }) => ({
+    expanded: !expanded,
+  }));
+
+  handleClick = _id => () => {
     const { navigation } = this.props;
     const { route } = analysis.filter(({ id }) => id === _id)[0];
     navigation.navigate(route, {
@@ -42,19 +55,31 @@ class Tests extends Component {
   }
 
   render() {
+    const { expanded } = this.state;
+
     return (
-      <S.Container>
-        {
-          analysis.map(({ id, value }) => (
-            <S.Item
-              key={id}
-              onPress={() => this.handleClick(id)}
-            >
-              <S.StyledText>{value}</S.StyledText>
-            </S.Item>
-          ))
-        }
-      </S.Container>
+      <List.Section>
+        <List.Accordion
+          title="Healthy Helper features"
+          left={props => <S.TitleIcon {...props} name="test" size={40} />}
+          expanded={expanded}
+          onPress={this.handlePress}
+        >
+          <S.Separator />
+          {
+            analysis.map(elem => (
+              <Fragment key={elem.id}>
+                <S.ListItem
+                  title={elem.value}
+                  onPress={this.handleClick(elem.id)}
+                  left={props => <S.ItemIcon {...props} name={elem.iconName} size={35} />}
+                />
+                <S.Separator />
+              </Fragment>
+            ))
+          }
+        </List.Accordion>
+      </List.Section>
     );
   }
 }
--- src/modules/overview/index.js
+++ src/modules/overview/index.js
@@ -1,60 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-// import { Text, TextInput, View, Button } from 'react-native';
 import { bindActionCreators, compose } from 'redux';
-// import CustomIcon from '../../components/CustomIcon.js'
-import { List, Checkbox } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import CustomIcon from '../../components/CustomIcon.js';
+import { List } from 'react-native-paper';
 
-// import axios from 'axios';
 import { actions } from '../../redux/auth';
+
 import * as S from './styled';
 
 class Overview extends Component {
-  static navigationOptions = () => ({ title: 'Overview' });
+  static navigationOptions = () => ({ title: 'Меню' });
 
-  // state = {
-  //   data: [],
-  // name: '',
-  // price: ''
-  // }
   state = {
     expanded: true,
   }
 
-  _handlePress = () => this.setState(({ expanded }) => ({
+  handlePress = () => this.setState(({ expanded }) => ({
     expanded: !expanded,
   }));
 
-  // componentDidMount() {
-  //   axios.get('https://fathomless-island-87115.herokuapp.com/products')
-  //     .then(response => response.data)
-  //     .then(responseJson => this.setState({ data: responseJson }))
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // }
-
-  // request = () => axios.get('https://fathomless-island-87115.herokuapp.com/products')
-  //   .then(response => response.data)
-  //   .then(responseJson => this.setState({ data: responseJson }))
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
-
-  handleClick = (i) => {
+  handleClick = i => () => {
     const { navigation } = this.props;
     switch (i) {
       case 0:
         navigation.navigate('Drugs', {
-          headerTitle: 'Overview',
+          headerTitle: 'Меню',
         });
         break;
       case 4:
         navigation.navigate('TestsForm', {
-          headerTitle: 'Overview',
+          headerTitle: 'Меню',
         });
         break;
       default:
@@ -63,84 +38,44 @@ class Overview extends Component {
     return null;
   }
 
-  // handleClickOnBtn = () => {
-  //   const { name, price } = this.state;
-  //   axios.post('https://fathomless-island-87115.herokuapp.com/products/create', { name, price })
-  //     .then(this.request)
-  //     // .then(responseJson => this.setState({ data: responseJson }))
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // }
-
   render() {
     const { overview } = this.props;
-    // const { data, name, price } = this.state;
+    const { expanded } = this.state;
 
     return (
-      <List.Section title="Accordions">
+      <List.Section>
         <List.Accordion
-          title="Uncontrolled Accordion"
-          left={props => <List.Icon {...props} icon="folder" />}
+          title="Healthy Helper features"
+          left={props => <S.TitleIcon {...props} name="cardiogram" size={40} />}
+          expanded={expanded}
+          onPress={this.handlePress}
         >
-          <List.Item title="First item" />
-          <List.Item title="Second item" />
-        </List.Accordion>
-
-        <List.Accordion
-          title="Controlled Accordion"
-          left={props => <CustomIcon {...props} name="medicine" size={50} /> }
-          expanded={this.state.expanded}
-          onPress={this._handlePress}
-        >
-          <List.Item title="First item" />
-          <List.Item title="Second item" />
+          <S.Separator />
+          {
+            overview.data.map((elem, indx) => (
+              <Fragment key={elem.id}>
+                <S.ListItem
+                  title={elem.name}
+                  onPress={this.handleClick(indx)}
+                  left={props => <S.ItemIcon {...props} name={elem.iconName} size={35} />}
+                />
+                <S.Separator />
+              </Fragment>
+            ))
+          }
         </List.Accordion>
       </List.Section>
     );
-    // return (
-    //   <S.Container>
-    //     <CustomIcon name='android' size={25} /> 
-    //     {/* <TextInput
-    //       style={{height: 40, border: 3, borderColor: 'black', width: 120}}
-    //       value={name}
-    //       placeholder="Name"
-    //       onChangeText={text => this.setState({ name: text })}
-    //     />
-    //     <TextInput
-    //       style={{height: 40, border: 3, borderColor: 'black', width: 120}}
-    //       value={price}
-    //       placeholder="Price"
-    //       onChangeText={text => this.setState({ price: text })}
-    //     />
-    //     <Button
-    //       onPress={this.handleClickOnBtn}
-    //       title="Post data"
-    //   /> */}
-    //     {/* {
-    //       overview.data.map((el, i) => (
-    //         <S.Item key={el.id} onPress={() => this.handleClick(i)}>
-    //           <S.StyledText>
-    //             {el.name}
-    //           </S.StyledText>
-    //         </S.Item>
-    //       ))
-    //     } */}
-    //   </S.Container>
-    // );
   }
 }
-
-Overview.defaultProps = {
-};
 
 Overview.propTypes = {
   overview: PropTypes.shape({}).isRequired,
   navigation: PropTypes.shape({}).isRequired,
 };
 
-const mapStateToProps = state => ({
-  overview: state.overview,
+const mapStateToProps = ({ overview }) => ({
+  overview,
 });
 
 const mapDispatchToProps = dispatch => ({
